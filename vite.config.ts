97cc0e3d28070e3d261c71be37bd1f1e6bfddc87
fernando <palaciosf416@gmail.com>
2025-01-27 18:24:52 -0600

    fer

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import gzipPlugin from "rollup-plugin-gzip";
-
 import dotenv from "dotenv";
 dotenv.config();
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Portafolio/',
-  plugins: [react()],
-  build: {
-    // ...
-    rollupOptions: {
-      // ...
-      plugins: [
-        // ...
-        gzipPlugin({
-          // Wähle die Dateitypen aus, die du komprimieren möchtest
-          filter: /\.(js|css|html|json|svg)$/,
-          // Gzip-Komprimierungseinstellungen (optional)
-          minSize: 1024, // Mindestgröße der komprimierten Datei in Bytes
-        }),
-      ],
+
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/Portafolio/' : '/',
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        plugins: [
+          gzipPlugin({
+            filter: /\.(js|css|html|json|svg)$/,
+            minSize: 1024,
+          }),
+        ],
+      },
     },
-  },
+  };
 });
